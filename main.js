5a3a1b472b53d6604aebef5cf2f788d48db2e818
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-12-30 17:38:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const path = require('path');
 
 // TODO: Use path.join to join two file path segments
+const segment1 = process.argv[2];
+const segment2 = process.argv[3];
+
+// Check if both segments are provided
+if (!segment1 || !segment2) {
+  console.error("Error: Please provide two file path segments as arguments.");
+  process.exit(1);
+}
+
+// Use path.join to join two file path segments
+const joinedPath = path.join(segment1, segment2);
+
 
 // TODO: Print the resulting path to the console
+console.log("Joined Path:", joinedPath);
